c76e3c18e2e0d1c0b21348cc71334e55db93452c
andreburto <tuglyraisin@gmail.com>
2026-05-10 18:32:17 -0500

    Debug workflow B

--- src/upload.py
+++ src/upload.py
@@ -21,9 +21,7 @@ def main():
         sys.exit(1)
 
     # Example usage of upload_file_to_s3
-    file_path = os.path.join(
-        os.path.dirname(os.path.abspath(os.path.curdir)), 
-        sys.argv[1])  # Path to the file you want to upload
+    file_path = os.path.join(os.path.curdir, sys.argv[1])  # Path to the file you want to upload
 
     if upload_to_s3(file_path, s3_bucket, sys.argv[1], aws_access_key, aws_secret_key):
         print(f"File {file_path} uploaded to S3 bucket https://{s3_bucket}/{sys.argv[1]}")
